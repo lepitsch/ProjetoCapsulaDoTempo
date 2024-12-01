2a6eb64273000318c42efcf21cf45aee20b04225
lepitsch <72175601+lepitsch@users.noreply.github.com>
2024-12-01 18:23:05 -0300

    alterando middleware

--- web/src/middleware.ts
+++ web/src/middleware.ts
@@ -6,14 +6,15 @@ export function middleware(request: NextRequest) {
   const token = request.cookies.get('token')?.value;
 
   if (!token) {
-    return NextResponse.redirect(signInURL, {
-      headers: {
-        'Set-Cookie': `redirectTo=${request.url}; Path=/; HttpOnly; Secure; max-age=300;`,
-        'Access-Control-Allow-Origin': '*', // Permite que qualquer origem acesse
-        'Access-Control-Allow-Methods': 'GET, POST, OPTIONS', // Métodos permitidos
-        'Access-Control-Allow-Headers': 'Content-Type, Authorization', // Cabeçalhos permitidos
-      },
-    });
+    // Redireciona o navegador para a URL de login do GitHub
+    const redirectResponse = NextResponse.redirect(signInURL);
+
+    redirectResponse.headers.set(
+      'Set-Cookie',
+      `redirectTo=${request.url}; Path=/; HttpOnly; Secure; max-age=300;`
+    );
+
+    return redirectResponse;
   }
 
   return NextResponse.next();
