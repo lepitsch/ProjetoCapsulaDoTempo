f0844681d1a34de61e0fc746a29b7ab6c9fae517
lepitsch <72175601+lepitsch@users.noreply.github.com>
2024-12-03 20:57:54 -0300

    Alteração no botão criar agora

--- web/src/components/EmptyMemories.tsx
+++ web/src/components/EmptyMemories.tsx
@@ -1,9 +1,12 @@
+import Cookie from 'js-cookie';
+
 export function EmptyMemories() {
+  const token = Cookie.get('token')
   return (
     <div className="flex flex-1 items-center justify-center p-16">
       <p className="w-[360px] text-center leading-relaxed">
         Você ainda não registrou nenhuma memória, comece a {''}
-        <a href='/memories/new' className="underline hover:text-gray-50">
+        <a href={`${token? 'memories/new' : `https://github.com/login/oauth/authorize?client_id=${process.env.NEXT_PUBLIC_GITHUB_CLIENT_ID}`}`} className="underline hover:text-gray-50">
           criar agora
         </a>
         !
